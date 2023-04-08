206cb984af3391f308afb3dd09de2645ab355f6a
nick.squdge <nick@squdge.me>
2023-04-09 00:11:40 +0100

    feature: Added Admin viewing of secret santas for a user

--- src/DiscordSecretSanta.Core/ViewModels/UserViewModel.cs
+++ src/DiscordSecretSanta.Core/ViewModels/UserViewModel.cs
@@ -8,6 +8,21 @@ public class UserViewModel
     public string UserId { get; set; } = string.Empty;
     public bool IsAdmin { get; set; } = false;
     public string WishlistUrl { get; set; } = string.Empty;
+    public SecretSantaStatus SecretSantaStatus { get; set; } = SecretSantaStatus.Unassigned;
+    public string SecretSantaUserId { get; set; } = string.Empty;
+
+    public static UserViewModel Unknown()
+        => new ()
+        {
+            IsAdmin = false,
+            Name = "??????",
+            AvatarId = string.Empty,
+            UserId = string.Empty,
+            WishlistUrl = string.Empty,
+            DiscordTagId = string.Empty,
+            SecretSantaUserId = string.Empty,
+            SecretSantaStatus = SecretSantaStatus.Unassigned
+        };
 
     public UserViewModel()
     {
@@ -22,5 +37,7 @@ public class UserViewModel
         DiscordTagId = user.DiscordId;
         UserId = user.UserId.Value;
         IsAdmin = user.IsAdmin;
+        SecretSantaUserId = user.SecretSantaUserId?.Value ?? string.Empty;
+        SecretSantaStatus = user.SecretSantaStatus;
     }
 }

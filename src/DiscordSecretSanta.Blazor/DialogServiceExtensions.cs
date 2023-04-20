6e8db4d6d45c8b9cc668c52e8b58e32cfbf1a450
nick.squdge <nick@squdge.me>
2023-04-20 17:30:09 +0100

    feature: Added in changing of status of a secret santa

---
+++ src/DiscordSecretSanta.Blazor/DialogServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Blazorise;
+using DiscordSecretSanta.Blazor.Shared;
+using DiscordSecretSanta.Core;
+using Microsoft.AspNetCore.Components;
+
+namespace DiscordSecretSanta.Blazor;
+
+public static class DialogServiceExtensions
+{
+    public static Task<ModalInstance> ShowSelectStatusModal(this IModalService modalService, SecretSantaStatus current, IEnumerable<SecretSantaStatus> available, Func<SecretSantaStatus, Task> onSelect)
+    {
+        return modalService.Show<SelectStatusModal>(x =>
+            {
+                x.Add(z => z.Current, current);
+                x.Add(z => z.AvailableStatuses, available);
+                x.Add(z => z.OnSelect, onSelect); 
+            }, 
+            new ModalInstanceOptions()
+            {
+                UseModalStructure = false,
+                Centered = true
+            }
+        );
+    }
+}
---
+++ src/DiscordSecretSanta.Blazor/StatusHelper.cs
@@ -0,0 +1,29 @@
+﻿using Blazorise;
+using DiscordSecretSanta.Blazor.Shared;
+using DiscordSecretSanta.Core;
+using Microsoft.Extensions.Localization;
+
+namespace DiscordSecretSanta.Blazor;
+
+internal static class StatusHelper
+{
+    public static Color GetColourForStatus(SecretSantaStatus? status)
+        => status switch
+        {
+            SecretSantaStatus.Unassigned => Color.Danger,
+            SecretSantaStatus.Arrived => Color.Success,
+            SecretSantaStatus.Assigned => Color.Warning,
+            SecretSantaStatus.Posted => Color.Info,
+            _ => Color.Danger
+        };
+    
+    public static string GetShortText(this IStringLocalizer<SharedResources> translate, SecretSantaStatus? status)
+        => status switch
+        {
+            SecretSantaStatus.Unassigned => translate["No Secret Santa yet"],
+            SecretSantaStatus.Arrived => translate["Gift has arrived"],
+            SecretSantaStatus.Assigned => translate["Secret Santa assigned"],
+            SecretSantaStatus.Posted => translate["Secret Santa gift en-route"],
+            _ => translate["No status set"]
+        };
+}

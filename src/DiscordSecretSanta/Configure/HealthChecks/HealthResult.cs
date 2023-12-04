04161df85ac5c1b515cfe218100678d9e64949c2
nick.squdge <nick@squdge.me>
2023-12-04 18:28:50 +0000

    fix: fixed issue with health result and unit tests

--- src/DiscordSecretSanta/Configure/HealthChecks/HealthResult.cs
+++ src/DiscordSecretSanta/Configure/HealthChecks/HealthResult.cs
@@ -4,9 +4,30 @@ namespace DiscordSecretSanta.Configure.HealthChecks;
 
 public sealed record HealthResult
 {
-    [JsonPropertyName("status")] public string Status { get; set; } = string.Empty;
+    [JsonPropertyName("status")]
+    public string Status { get; set; } = string.Empty;
 
-    [JsonPropertyName("duration")] public TimeSpan Duration { get; set; }
+    [JsonPropertyName("totalDuration")]
+    public TimeSpan Duration { get; set; }
 
-    [JsonPropertyName("entries")] public ICollection<HealthInfo> Entries { get; set; } = new List<HealthInfo>();
+    [JsonPropertyName("entries")]
+    public Dictionary<string, HealthInfo> Entries { get; set; } = new();
+
+    public sealed record HealthInfo
+    {
+        [JsonPropertyName("data")]
+        public object? Data { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = string.Empty;
+
+        [JsonPropertyName("duration")]
+        public TimeSpan Duration { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; } = string.Empty;
+
+        [JsonPropertyName("tags")]
+        public string[] Tags { get; set; } = Array.Empty<string>();
+    }
 }
